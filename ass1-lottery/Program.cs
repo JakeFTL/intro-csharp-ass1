827528f368bdc6ae91f5234801b5be46c7701cda
Jake <176844671+LgndryJake@users.noreply.github.com>
2025-03-03 20:47:59 +0800

    implementation of LotteryResult class with Compare, BinaryCompare and Display methods.

--- ass1-lottery/Program.cs
+++ ass1-lottery/Program.cs
@@ -3,18 +3,23 @@ string userName = Welcome.GetName();
 int[] userNumbers = NumberSelection.UserSelection();
 int[] randomNumbers = NumberSelection.RandomSelection();
 
-/*Console.WriteLine("Selected Numbers:");
+Console.WriteLine("Selected Numbers:");
 foreach (int number in userNumbers)
 {
     Console.Write("{0} ", number);
 }
+
 Console.WriteLine("");
-Console.WriteLine("\nGenerated Numbers:");
+Console.WriteLine("\nThe correct numbers were:");
 foreach (int number in randomNumbers)
 {
     Console.Write("{0} ", number);
 }
-Console.WriteLine("");*/
+Console.WriteLine("");
+
+//LotteryResult.Compare(userNumbers, randomNumbers);
+LotteryResult.BinaryCompare(userNumbers, randomNumbers);
+LotteryResult.Display(userNumbers.Length);
 
 static class Welcome
 {
@@ -53,7 +58,7 @@ static class Welcome
 class NumberSelection
 {
     static int rangeMin = 10; // Set lowest value that the program can select from.
-    static int rangeMax = 50; // Set highest value that the program can select from.
+    static int rangeMax = 20; // Set highest value that the program can select from.
     static int entries = 5; // The amount of numbers to be selected by user & program.
     public static int[] userNumbers = new int[entries];
     public static int[] randomNumbers = new int[entries];
@@ -101,17 +106,74 @@ class NumberSelection
         for (int i = 0; i < entries; i++)
         {
             Random rnd = new Random();
-            int randomNumber = rnd.Next(rangeMin, rangeMax);
+            int randomNumber = rnd.Next(rangeMin, rangeMax + 1);
             randomNumbers[i] = randomNumber;
         }
         return randomNumbers;
     }
 }
 
-//static class LotteryNumbers
-//{
-//    public static compare()
-//    {
-//        return;
-//    }
-//}
+static class LotteryResult
+{
+    static int correctCount = 0;
+    public static int Compare(int[] userNumbers, int[] randomNumbers)
+    {
+        for (int userNum = 0; userNum < userNumbers.Length; userNum++)
+        {
+            for (int randNum = 0; randNum < randomNumbers.Length; randNum++)
+            {
+                if (userNumbers[userNum] == randomNumbers[randNum])
+                {
+                    correctCount++;;
+                    break;
+                }
+            }
+        }
+        return correctCount;
+    }
+
+    public static int BinaryCompare(int[] userNumbers, int[] randomNumbers)
+    {
+        Array.Sort(userNumbers);
+        Array.Sort(randomNumbers);
+
+        foreach (int userNum in userNumbers)
+        {
+            int low = 0;
+            int high = randomNumbers.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (randomNumbers[mid] == userNum)
+                {
+                    correctCount++;
+                    break;
+                }
+                if (randomNumbers[mid] > userNum)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+        }
+        return correctCount;
+    }
+
+    public static void Display(int entries)
+    {
+        if (correctCount == entries)
+        {
+            Console.WriteLine("Congratulations, you won!");
+        }
+        else
+        {
+            Console.WriteLine("\nSorry, no winnings! You got {0} out of {1} correct\n", correctCount, entries);
+            Console.WriteLine("THANKS FOR PLAYING! :)");
+        }
+    }
+}
